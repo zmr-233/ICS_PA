81b7d74a0767ed5756c8f081e8a4f0eac37eb4d2
tracer-ics2023 <tracer@njuics.org>
2024-03-07 19:22:18 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-N18L6FL3 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:22:18 up  5:11,  4 users,  load average: 0.42, 0.22, 0.13

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -76,6 +76,7 @@ static int cmd_info(char* args){
 /*x N EXPR 求出表达式EXPR的值, 将结果作为起始内存地址
 x 10 $esp 以十六进制形式输出连续的N个4字节*/
 static int cmd_x(char* args){
+  if(args == NULL) {printf("No arguments\n"); return 0;}
   printf("|%s|\n",args);
   return 0;
 }
