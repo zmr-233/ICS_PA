c73eb34bf094d9d76ad3200cdd2473eb514a564c
tracer-ics2023 <tracer@njuics.org>
2024-03-08 15:09:44 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-N18L6FL3 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:09:44 up  1:35,  2 users,  load average: 0.06, 0.05, 0.09

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -79,13 +79,18 @@ static int cmd_x(char* args){
   Log("args = %s", args);
   if(args == NULL) { puts("No arguments"); return 0;}
   char *str1, *str2, *str3, *saveptr;
+  
   str1 = strtok_r(args, " ", &saveptr);
   Log("str1 = %s",str1);
-  //str2 = strtok_r(NULL, " ", &saveptr);
+
   str2 = saveptr;
   Log("str2 = %s",str2);
+
   if(str1 == NULL || str2 == NULL) { puts("Invalid arguments"); return 0;}
-  if((str3 = strtok_r(NULL, " ", &saveptr)) != NULL)  printf("Too many arguments :%s\n",str3);
+  
+  str3 = strtok_r(NULL, "\"", &saveptr);
+  Log("str3 = %s",str3);
+  //printf("Too many arguments :%s\n",str3);
 
   int N;
   sscanf(str1, "%d", &N);
