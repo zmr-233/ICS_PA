ecced16154cf29464bee819a91410a44e4d94319
tracer-ics2023 <tracer@njuics.org>
2024-03-08 15:58:25 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-N18L6FL3 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:58:25 up  2:24,  2 users,  load average: 0.03, 0.18, 0.16

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -103,7 +103,7 @@ static int cmd_x(char* args){
   int N; sscanf(str1, "%d", &N);
   bool success = true;  vaddr_t addr = 0;
   if(str2 != str3)  addr = expr(str3, &success);
-  else addr = sscanf(str2, HEX_SCWORD, &addr); 
+  else sscanf(str2, HEX_SCWORD, &addr); 
   //TODO: 这里如何根据平台不同进行分离是个好问题  ^^^^^^^^^^^^^^^^^^^^
 
   if(success == false) { puts("Invalid expression"); return 0;}
