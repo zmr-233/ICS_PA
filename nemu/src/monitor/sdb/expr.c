01d2dd10523cb092913ec929e26685f0643282a1
tracer-ics2023 <tracer@njuics.org>
2024-03-09 17:23:45 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-N18L6FL3 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:23:45 up  3:11,  1 user,  load average: 0.30, 0.18, 0.11

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -212,6 +212,7 @@ static int getMainOp(int p, int q, bool* success){
 
     if(cur_prio <= op_prio) {op = i; op_prio = cur_prio;}
   }
+  if(op < p || op > q)Assert(*success == false,"Bad expression : main op = %d",op);
   return op;
 }
 
