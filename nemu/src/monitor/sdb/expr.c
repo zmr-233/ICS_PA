13c2b9964ee3e6afb4c62db236ffb7587e0d4717
tracer-ics2023 <tracer@njuics.org>
2024-03-10 05:05:41 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-N18L6FL3 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  05:05:41 up 14:53,  4 users,  load average: 0.05, 0.07, 0.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -165,8 +165,8 @@ expr 函数的目标是实现一个表达式求值器，但是目前的代码只
 static void print_tokens(){
   Log("Print tokens: ");
   for(int i=0; i<nr_token; i++)
-    if(tokens[i].type > 256) Log(" >>%d", tokens[i].type);
-    else Log(" >>%d: \\|%s\\|", tokens[i].type, tokens[i].str);
+    if(tokens[i].type < 256) Log(" >>%c", (char)tokens[i].type);
+    else Log(" >>%d: |%s|", tokens[i].type, tokens[i].str);
   Log("---------------");
 }
 
