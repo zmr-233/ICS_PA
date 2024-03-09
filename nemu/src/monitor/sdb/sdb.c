1031fd792191a98746676f78c659de28ac065d8a
tracer-ics2023 <tracer@njuics.org>
2024-03-10 02:09:48 +0800

    >  compile NEMU 221220000 张三 Linux LAPTOP-N18L6FL3 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:09:48 up 11:57,  3 users,  load average: 0.39, 0.19, 0.12

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -132,7 +132,7 @@ static int cmd_x(char* args){
 https://nju-projectn.github.io/ics-pa-gitbook/ics2024/1.6.html
 p $eax + 1*/
 static int cmd_p(char* args){
-  //Log("args :%s", args);
+  Log("args :%s", args);
   if(args == NULL) { puts("No arguments"); return 0;}
   char * saveptr=args;
   char * str1;
@@ -143,7 +143,7 @@ static int cmd_p(char* args){
       Log("if(str1[0] == \"\\\") Str1 :%s, saveptr :%s",str1,saveptr);
     }else{
       str1 = strtok_r(NULL, "\"", &saveptr);
-      //Log("else: Str1 :%s, saveptr :%s",str1,saveptr);
+      Log("else: Str1 :%s, saveptr :%s",str1,saveptr);
     }
   }while(str1!=NULL);
   
